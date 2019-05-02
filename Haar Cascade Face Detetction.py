f33ae9aa943a30125a8effc2a537c76edae7c9c0
Deeksha S <42448048+DeekshaS0171@users.noreply.github.com>
2019-05-02 09:58:18 -0500

    Update Haar Cascade Face Detetction.py

--- Haar Cascade Face Detetction.py
+++ Haar Cascade Face Detetction.py
@@ -11,10 +11,10 @@ Created on Wed Mar 27 11:30:55 2019
 import cv2
 import numpy as np
 
-faces = cv2.imread("C:\\Users\\deeks\\Desktop\\Adsoft\CV\\Ex_Files_OpenCV_Python_Dev\\Exercise Files\\Ch04\\04_05 Begin\\faces.jpeg",1)
+faces = cv2.imread("C..........\\faces.jpeg",1)
 gray = cv2.cvtColor(faces,cv2.COLOR_BGR2GRAY)
 
-Cascade_faces = cv2.CascadeClassifier("C:\\Users\\deeks\\Desktop\\Adsoft\\CV\\Ex_Files_OpenCV_Python_Dev\\Exercise Files\\Ch04\\04_05 Begin\\haarcascade_frontalface_default.xml")
+Cascade_faces = cv2.CascadeClassifier("...............\\haarcascade_frontalface_default.xml")
 
 Detect_Face = Cascade_faces.detectMultiScale(gray,scaleFactor = 1.05, minNeighbors = 5, minSize = (40,40))
 #This returs a list of locations where the faces were detected
